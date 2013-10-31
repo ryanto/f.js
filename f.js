70829fd80ab9781314a0fd23b51006020f380b75
Ryan <ryanto@gmail.com>
2013-10-31 02:57:45 -0400

    partial / no partial to main function

--- f.js
+++ f.js
@@ -1,8 +1,31 @@
 window.F = function(context, f) {
-  var args = Array.prototype.slice.call(arguments, 2);
-  return function() {
-    var additionalArgs = Array.prototype.slice.call(arguments, 0),
-        func = (typeof f == "string" ? context[f] : f);
-    return func.apply(context, args.concat(additionalArgs));
+  var args = Array.prototype.slice.call(arguments, 2),
+      toCall = (typeof f == "string" ? context[f] : f);
+
+  var full = function() {
+    return F.apply(toCall, context, args);
   };
+
+  var partial = function() {
+    var additionalArgs = Array.prototype.slice.call(arguments, 0);
+    return F.apply(toCall, context, args.concat(additionalArgs));
+  };
+
+  full.partial = partial;
+
+  return full;
 };
+
+F.K = function(context, f) {
+  f.apply(context);
+  return context;
+};
+
+F.partial = function() {
+  return F.apply(null, arguments).partial;
+};
+
+F.apply = function(f, context, args) {
+  return f.apply(context, args);
+};
+
